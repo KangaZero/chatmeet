dfbb552113b4c43b2643a1dfe9468dd7695a1596
Samuel <samuelyongw@gmail.com>
2023-02-02 15:35:11 +1100

    Sign in and sign out working

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,40 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-// import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next'
+import { signIn, signOut, useSession } from 'next-auth/react';
 
-const inter = Inter({ subsets: ['latin'] })
+const Home: NextPage = () => {
 
-export default function Home() {
+  const { data } = useSession();
+
+  console.log('daataa', data);
+
+
   return (
       <div>
-        Hi
+        {data?.user ?
+          <button 
+          onClick={()  => signOut()}
+          > 
+            Sign Out 
+          </button>
+        :
+          <button 
+          onClick={()  => signIn('google')}
+          > 
+            Sign In 
+          </button>
+}
+          <div>
+            <h1>{data?.user?.name}</h1>
+            <img
+              src={data?.user?.image as string}
+              alt={data?.user?.name as string}
+              />
+          </div>
       </div>
   )
 }
+
+export default Home;
