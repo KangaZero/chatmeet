247eef94e7bcde9601ac8f4dcdd853fbc509cc6c
Samuel <samuelyongw@gmail.com>
2023-02-05 14:11:06 +1100

    added template subscription set up on index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,8 @@ import http from 'http';
 import * as dotenv from 'dotenv';
 import { getSession } from 'next-auth/react';
 import { PrismaClient } from '@prisma/client';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
@@ -18,6 +20,8 @@ async function main() {
   // Below, we tell Apollo Server to "drain" this httpServer,
   // enabling our servers to shut down gracefully.
   const httpServer = http.createServer(app);
+  const prisma = new PrismaClient();
+
 
   const schema = makeExecutableSchema(
    { typeDefs,
@@ -30,7 +34,14 @@ async function main() {
     credentials: true,
   }
 
-  const prisma = new PrismaClient();
+  // Create our WebSocket server using the HTTP server we just set up.
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql/subscriptions',
+});
+
+// Save the returned server's info so we can shutdown this server later
+const serverCleanup = useServer({ schema }, wsServer);
 
   // Same ApolloServer initialization as before, plus the drain plugin
   // for our httpServer.
@@ -46,8 +57,19 @@ async function main() {
         return { session, prisma };
     },
     plugins: [
+      // Proper shutdown for the HTTP server.
         ApolloServerPluginDrainHttpServer({ httpServer }), 
-        ApolloServerPluginLandingPageLocalDefault({ embed: true })
+        ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+           // Proper shutdown for the WebSocket server.
+        {
+          async serverWillStart() {
+            return {
+              async drainServer() {
+                await serverCleanup.dispose();
+              },
+            };
+          },
+        },
     ],
   });
 
